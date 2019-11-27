c8301fa05c4a8159e11c35850de1cb56e70cd3d6
JauntZ <billkill90@icloud.com>
2019-11-27 20:33:43 +0100

    Add method to convert CSV to JS array

--- src/App.js
+++ src/App.js
@@ -29,6 +29,31 @@ const App = () => {
 
     fetchData();
   }, []);
+
+  // Adapted from:  http://techslides.com/convert-csv-to-json-in-javascript
+  const csvToJSON = (csv) => {
+    const lines = csv.split("\n");
+    const headers = lines[0].split(",");
+
+    const result = lines.map((line, i) => {
+      if (i === 0) {
+        return null;
+      }
+
+      const obj = {};
+      const currentline = lines[i].split(",");
+      headers.forEach((header, i) => obj[headers[i]] = currentline[i]);
+      return obj;
+
+    }).filter(r => (r !== null) && (r.Date.length > 0));
+
+    // console.log("resultz", result);
+
+    setMockData(result);
+    collectMetricsByDate(result);
+    setFilterOptions(result);
+  }
+
   return (
     <div className="App">
       <Sidebar />
