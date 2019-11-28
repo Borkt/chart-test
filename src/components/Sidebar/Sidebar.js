da69b4329fcefc75b0813d8bd19bf1fa44a36841
JauntZ <billkill90@icloud.com>
2019-11-28 13:13:17 +0100

    Add React Select to Sidebar component and add basic styling

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Select from 'react-select';
+
 import './Sidebar.css';
 
-export const Sidebar = () => {
+export const Sidebar = ({
+  activeCampaignFilters,
+  activeDatasourceFilters,
+  campaignFilterOptions,
+  datasourceFilterOptions,
+  setCampaignFilter,
+  setDatasourceFilter,
+}) => {
+
   return (
     <div className="Sidebar">
-      SSidebare
+      <Select
+        className="Sidebar-react-select"
+        classNamePrefix="Sidebar-react-select"
+        isSearchable={true}
+        isMulti={true}
+        value={activeCampaignFilters}
+        onChange={setCampaignFilter}
+        options={campaignFilterOptions}
+      />
+
+      <Select
+        className="Sidebar-react-select"
+        classNamePrefix="Sidebar-react-select"
+        isSearchable={true}
+        isMulti={true}
+        value={activeDatasourceFilters}
+        onChange={setDatasourceFilter}
+        options={datasourceFilterOptions}
+      />
     </div>
   );
 }
+
+Sidebar.propTypes = {
+  activeCampaignFilters: PropTypes.array.isRequired,
+  activeDatasourceFilters: PropTypes.array.isRequired,
+  campaignFilterOptions: PropTypes.array.isRequired,
+  datasourceFilterOptions: PropTypes.array.isRequired,
+  setCampaignFilter: PropTypes.func.isRequired,
+  setDatasourceFilter: PropTypes.func.isRequired,
+};
